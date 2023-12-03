733ed5f6193a0a99a441038f906ec09f0b7bc00a
madomah2035 <kwamemusah51@gmail.com>
2023-12-03 11:04:35 -0800

    lower to upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,10 +2,8 @@
 def uppercase(str):
     for alpha in str:
         if ord('a') <= ord(alpha) <= ord('z'):
-            upper = chr(ord(alpha) - (ord('a') - ord('A')))
-            print("{}".format(upper), end="")
+            upper = chr(ord(alpha) - (ord('a') - ord('A'))
         else:
             upper = alpha
-            print("{}".format(upper), end="")
-
-    print()
+        print("{}".format(upper), end="")
+        print("{}".format("\n"))
