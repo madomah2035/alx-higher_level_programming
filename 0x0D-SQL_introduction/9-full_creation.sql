b075e510d602ee903a155882fbe77ccef167c599
madomah2035 <kwamemusah51@gmail.com>
2024-02-14 13:36:10 -0800

    full_creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
--- This creates a second table in the db and inserts data into it.
+-- This creates a second table in the db and inserts data into it
 
 CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(1, "John", 10);
