6836a3632c151330cf4701e17bfbe8346354e379
madomah2035 <kwamemusah51@gmail.com>
2024-02-14 12:50:26 -0800

    first_table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- This creates a table in the current db called `first_name` with attributes, ID: INT, and  name: VARCHAR(256)
+
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
+
