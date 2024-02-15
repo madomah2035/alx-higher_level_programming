553790c5c1a07f199cc4de38495d5b17eb24d555
madomah2035 <kwamemusah51@gmail.com>
2024-02-15 06:33:34 -0800

    shows by genre

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,13 +1,8 @@
--- uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
--- uses a database to list all rows not linked to one row
-SELECT name
-FROM tv_genres
-WHERE name NOT IN
-(SELECT name
-FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = 'Dexter')
-GROUP BY name
-ORDER BY name ASC;
+-- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+-- lists all rows of a table linked to another table
+SELECT title, name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY title ASC, name ASC;
 
